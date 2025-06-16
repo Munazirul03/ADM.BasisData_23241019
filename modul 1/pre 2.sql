5cb9ca882983fa97c1ab35625ff4d8a2e4b926aa
Munazirul03 <145947248+Munazirul03@users.noreply.github.com>
2025-06-16 10:28:17 +0800

    Add files via upload

---
+++ modul 1/pre 2.sql
@@ -0,0 +1,20 @@
+-- Nama : Andrian Maulana
+-- Nim : 23241020
+-- Kelas : A 
+
+-- melihat database dalam MySQL
+SHOW DATABASES;
+
+-- memilih database yang digunakan
+USE mysql;
+
+-- menampilkan tabel dalam database mysql
+SHOW TABLES;
+
+-- mendeskripsikan tabel user
+DESCRIBE user;
+
+-- menampilkan isi data dari kolom Host, User, Password
+SELECT Host, User, Password FROM user;
+
+
